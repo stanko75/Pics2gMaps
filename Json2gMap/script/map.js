9b2e6e35563d8505095e08f6efc351b728ea3e5f
stanko.milosev <stanko@milosev.com>
2018-09-13 22:22:54 +0200

    - display on gMap

--- Json2gMap/script/map.js
+++ Json2gMap/script/map.js
@@ -1,3 +1,30 @@
 $.getJSON("myJosn.json", function (data) {
-        document.write("tesrt");
-    });
+	var mapOptions,
+        mapCanvas,
+        map;
+
+    mapOptions = {
+        zoom: 6,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: { lat: 50.74388888888889, lng: 7.1522222222222229},		
+    };
+		
+	mapCanvas = document.getElementById('map-canvas');
+    if (!map) {
+        map = new google.maps.Map(mapCanvas, mapOptions);
+	}
+	
+		
+	data.forEach(function(file) {
+	var myLatlng = new google.maps.LatLng(file.Latitude, file.Longitude);		
+		
+
+	
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: file.FileName
+		});
+    })
+        //document.write("tesrt");
+});
