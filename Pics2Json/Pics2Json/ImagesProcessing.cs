a85b3a20b4afc77f9c2043b172f4280f0c16a5f8
stanko.milosev <stanko@milosev.com>
2019-05-21 20:01:27 +0200

    - added query if picture is needed to rotate after resizing

--- Pics2Json/Pics2Json/ImagesProcessing.cs
+++ Pics2Json/Pics2Json/ImagesProcessing.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.Linq;
 
 namespace Pics2Json
 {
@@ -47,7 +48,57 @@ namespace Pics2Json
         encoderParameters = new EncoderParameters(1);
         encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality,
                          100L);
-        thumbnail.RotateFlip(RotateFlipType.Rotate90FlipNone);
+
+        int OrientationKey = 0x0112;
+        const int NotSpecified = 0;
+        const int NormalOrientation = 1;
+        const int MirrorHorizontal = 2;
+        const int UpsideDown = 3;
+        const int MirrorVertical = 4;
+        const int MirrorHorizontalAndRotateRight = 5;
+        const int RotateLeft = 6;
+        const int MirorHorizontalAndRotateLeft = 7;
+        const int RotateRight = 8;
+
+        if (image.PropertyIdList.Contains(OrientationKey))
+        {
+          if (image.PropertyIdList.Contains(OrientationKey))
+          {
+            var orientation = (int)image.GetPropertyItem(OrientationKey).Value[0];
+            switch (orientation)
+            {
+              case NotSpecified: // Assume it is good.
+              case NormalOrientation:
+                // No rotation required.
+                break;
+              case MirrorHorizontal:
+                thumbnail.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                break;
+              case UpsideDown:
+                thumbnail.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                break;
+              case MirrorVertical:
+                thumbnail.RotateFlip(RotateFlipType.Rotate180FlipX);
+                break;
+              case MirrorHorizontalAndRotateRight:
+                thumbnail.RotateFlip(RotateFlipType.Rotate90FlipX);
+                break;
+              case RotateLeft:
+                thumbnail.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                break;
+              case MirorHorizontalAndRotateLeft:
+                thumbnail.RotateFlip(RotateFlipType.Rotate270FlipX);
+                break;
+              case RotateRight:
+                thumbnail.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                break;
+              default:
+                throw new NotImplementedException("An orientation of " + orientation + " isn't implemented.");
+            }
+          }
+        }
+
+        //thumbnail.RotateFlip(RotateFlipType.Rotate90FlipNone);
         thumbnail.Save(saveTo, info[1], encoderParameters);
         log.WriteLog($"Thumbnail from file: {originalFilename} created in {saveTo}");
       }
