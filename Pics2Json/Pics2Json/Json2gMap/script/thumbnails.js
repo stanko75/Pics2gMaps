987ad646550abf8cf788f51ec5abf15f365f55d1
stanko.milosev <stanko@milosev.com>
2019-05-26 23:01:08 +0200

    - added function for zooming map

--- Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
+++ Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
@@ -1,7 +1,11 @@
+function zoomMap(x) {
+    debugger;
+}
+
 $.getJSON("/*picsJson*/Thumbs.json", function (data) {
 	var thumbs;
 	thumbs = $('#thumbnails');
 	data.forEach(function(file) {
-		$('#thumbnails').append('<img src="' + file.FileName + '">');
+        $('#thumbnails').append('<img onmouseover = "zoomMap(this)" src="' + file.FileName + '">');
 	})
 })
