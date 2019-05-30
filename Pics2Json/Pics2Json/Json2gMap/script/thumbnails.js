dd88e7608a4bc0839c994d8a9133c0b5d55afb07
stanko.milosev <stanko@milosev.com>
2019-05-30 20:11:12 +0200

    - focus position on map when mouse over, when clicked stop executing mouseover event

--- Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
+++ Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
@@ -4,10 +4,20 @@ $.getJSON("/*picsJson*/Thumbs.json", function (data) {
     data.forEach(function (val, key) {
         thumbs.append('<img id="' + key + '"' + ' src=' + val.FileName + ">");
         thumbs.on('mouseover', '#' + key, function () {
-            window.milosev.map.setZoom(20);
-        })
+            if (!clicked) {
+                $('#' + key).css("border", "14px solid #333");
+                window.milosev.map.setZoom(20);
+                window.milosev.map.setCenter(new google.maps.LatLng(val.Latitude, val.Longitude), 13);
+            }
+        });
         thumbs.on('mouseout', '#' + key, function () {
-            window.milosev.map.setZoom(15);
-        })		
+            if (!clicked) {
+                $('#' + key).css("border", "");
+                window.milosev.map.setZoom(15);
+            }
+        });
+        thumbs.on('click', '#' + key, function () {
+            clicked = !clicked;
+        });
 	})
 })
