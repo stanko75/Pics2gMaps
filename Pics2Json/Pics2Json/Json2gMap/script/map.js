3650d754e747499366ea7cdd9b7c5404d3806bf2
stanko.milosev <stanko@milosev.com>
2019-05-30 11:51:07 +0200

    - map introduced as global varible in window.milosev namespace

--- Pics2Json/Pics2Json/Json2gMap/script/map.js
+++ Pics2Json/Pics2Json/Json2gMap/script/map.js
@@ -1,38 +1,44 @@
-$.getJSON("/*picsJson*/", function (data) {
-	var mapOptions,
+(function (ns) {
+    var mapOptions,
         mapCanvas,
         map;
 
-    if (typeof google !== 'object')
-    {
-        setTimeout(function () {
-            location.reload();
-        }, 1000);
-    }
+    $.getJSON("/*picsJson*/", function (data) {
 
-    mapOptions = {
-        zoom: 6,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: { lat: 50.74388888888889, lng: 7.1522222222222229}
-    };
-		
-	mapCanvas = document.getElementById('map-canvas');
-    if (!map) {
-        map = new google.maps.Map(mapCanvas, mapOptions);
-	}
-		
-	data.forEach(function(file) {
-		var myLatlng = new google.maps.LatLng(file.Latitude, file.Longitude);		
-	
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			title: file.FileName,
-			url: file.FileName
-		});
-		
-		marker.addListener('click', function() {
-			window.open(marker.url,"_target")
-        });			
-    })
-});
+
+        if (typeof google !== 'object') {
+            setTimeout(function () {
+                location.reload();
+            }, 1000);
+        }
+
+        mapOptions = {
+            zoom: 6,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            center: { lat: 50.74388888888889, lng: 7.1522222222222229 }
+        };
+
+        mapCanvas = document.getElementById('map-canvas');
+        if (!map) {
+            map = new google.maps.Map(mapCanvas, mapOptions);
+        }
+
+        data.forEach(function (file) {
+            var myLatlng = new google.maps.LatLng(file.Latitude, file.Longitude);
+
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map,
+                title: file.FileName,
+                url: file.FileName
+            });
+
+            marker.addListener('click', function () {
+                window.open(marker.url, "_target")
+            });
+        })
+
+        ns.map = map;
+
+    });
+})(window.milosev);
