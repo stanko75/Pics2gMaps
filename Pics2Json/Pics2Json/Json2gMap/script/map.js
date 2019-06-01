d7b9786868b7c259f929a5997742aa3aa25cca20
stanko.milosev <stanko@milosev.com>
2019-06-01 11:20:32 +0200

    - scrollwheel set to true

--- Pics2Json/Pics2Json/Json2gMap/script/map.js
+++ Pics2Json/Pics2Json/Json2gMap/script/map.js
@@ -12,7 +12,7 @@
     function initMap() {
         map = new google.maps.Map(document.getElementById('map-canvas'), {
             center: { lat: 34.397, lng: 150.644 },
-            scrollwheel: false,
+            scrollwheel: true,
             zoom: 2
         });
         ns.map = map;
