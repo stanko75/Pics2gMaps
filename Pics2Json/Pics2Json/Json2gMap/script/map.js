08b92074eec9d8d3b4992a5f6a980318f517c44d
stanko.milosev <stanko@milosev.com>
2019-06-03 21:28:33 +0200

    - added try catch in creating maps

--- Pics2Json/Pics2Json/Json2gMap/script/map.js
+++ Pics2Json/Pics2Json/Json2gMap/script/map.js
@@ -3,18 +3,22 @@
     "use strict";
     var map;
 
-    setTimeout(function () {
-        if (typeof google !== 'object') {
-            location.reload();
-        }
-    }, 1000);
-
     function initMap() {
-        map = new google.maps.Map(document.getElementById('map-canvas'), {
-            center: { lat: 34.397, lng: 150.644 },
-            scrollwheel: true,
-            zoom: 2
-        });
+        try {
+            map = new google.maps.Map(document.getElementById('map-canvas'), {
+                center: { lat: 34.397, lng: 150.644 },
+                scrollwheel: true,
+                zoom: 2
+            });
+        }
+        catch (e) {
+            console.log(e);
+            setTimeout(function () {
+                if (typeof google !== 'object') {
+                    location.reload();
+                }
+            }, 1000);
+        }
         ns.map = map;
     }
 
