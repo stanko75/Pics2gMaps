0cf4c84b6624953342fcb779a90ec739b59b7261
stanko.milosev <stanko@milosev.com>
2019-06-03 21:40:29 +0200

    - whole block moved under try...catch

--- Pics2Json/Pics2Json/Json2gMap/script/pics2maps.js
+++ Pics2Json/Pics2Json/Json2gMap/script/pics2maps.js
@@ -1,8 +1,8 @@
 (function (ns) {
-	/*globals google, $*/
-	"use strict";
+    /*globals google, $*/
+    "use strict";
     $.getJSON("/*picsJson*/.json", function (data) {
-        data.forEach(function (file) { 
+        data.forEach(function (file) {
 
             try {
                 var picsLatLng = new google.maps.LatLng(file.Latitude, file.Longitude);
@@ -14,6 +14,24 @@
                     title: file.FileName,
                     url: file.FileName
                 });
+
+                marker.addListener('click', function () {
+                    window.open(marker.url, "_target");
+                });
+
+                bounds.extend(picsLatLng);
+                ns.map.fitBounds(bounds);
+
+                var zoomChangeBoundsListener =
+                    google.maps.event.addListenerOnce(ns.map, 'bounds_changed', function (event) {
+                        if (ns.map.getZoom()) {
+                            ns.map.setZoom(14);  // set zoom here
+                        }
+                    });
+
+                setTimeout(function () {
+                    google.maps.event.removeListener(zoomChangeBoundsListener);
+                }, 2000);
             }
             catch (e) {
                 console.log(e);
@@ -23,24 +41,6 @@
                     }
                 }, 1000);
             }
-
-			marker.addListener('click', function () {
-				window.open(marker.url, "_target");
-			});
-				
-			bounds.extend(picsLatLng);
-			ns.map.fitBounds(bounds);	
-			
-			var zoomChangeBoundsListener = 
-				google.maps.event.addListenerOnce(ns.map, 'bounds_changed', function(event) {
-					if ( ns.map.getZoom() ){
-						ns.map.setZoom(14);  // set zoom here
-					}
-				});
-
-			setTimeout(function(){
-				google.maps.event.removeListener(zoomChangeBoundsListener);
-			}, 2000);			
-		});
-	});
+        });
+    });
 })(window.milosev);
