e8397d6ccf1418e2f636885d46c176eaf7827ed5
Stanko Milosev <stanko.milosev@windowslive.com>
2020-10-18 13:00:40 +0200

    - mouseover and mouseout events are disabled, since they are annoying

--- Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
+++ Pics2Json/Pics2Json/Json2gMap/script/thumbnails.js
@@ -23,25 +23,25 @@
         thumbs = $('#thumbnails');
         data.forEach(function (val, key) {
             thumbs.append('<img id="' + key + '"' + ' src=' + val.FileName + ">");
-            thumbs.on('mouseover', '#' + key, function () {
-                if (!clicked) {
-                    $('#' + key).css("border", "14px solid #333");
-                    ns.map.setZoom(20);
-                    picsLatLng = new google.maps.LatLng(val.Latitude, val.Longitude);
-                    ns.map.setCenter(picsLatLng, 13);
-                    resizeMarkerIcon(70, val.FileName);
-                }
-            });
-            thumbs.on('mouseout', '#' + key, function () {
-                if (!clicked) {
-                    $('#' + key).css("border", "");
-                    ns.map.setZoom(/*zoom*/);
-                    resizeMarkerIcon(30, "");
-                }
-            });
+            //thumbs.on('mouseover', '#' + key, function () {
+            //    if (!clicked) {
+            //        $('#' + key).css("border", "14px solid #333");
+            //        ns.map.setZoom(20);
+            //        picsLatLng = new google.maps.LatLng(val.Latitude, val.Longitude);
+            //        ns.map.setCenter(picsLatLng, 13);
+            //        resizeMarkerIcon(70, val.FileName);
+            //    }
+            //});
+            //thumbs.on('mouseout', '#' + key, function () {
+            //    if (!clicked) {
+            //        $('#' + key).css("border", "");
+            //        ns.map.setZoom(/*zoom*/);
+            //        resizeMarkerIcon(30, "");
+            //    }
+            //});
             thumbs.on('click', '#' + key, function () {
                 //$('table[style*="width: 555px"]')
-                if (clicked && $('#' + key).css('borderWidth').toLowerCase() == '14px') {
+                if (clicked && $('#' + key).css('borderWidth').toLowerCase() === '14px') {
                     clicked = false;
                     $('#' + key).css("border", "");
                     ns.map.setZoom(15);
